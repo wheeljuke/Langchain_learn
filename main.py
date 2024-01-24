b7fa45823f27124e8a137a31311e7d755271e3c6
Stephen200274 <stephen200274@gmail.com>
2024-01-24 21:09:57 +0800

    Parsing the JSON
    Parsed the JSON and turned it into a list of dict. Further processed by pandas. I have successfully turned the questions into DataFrame format. And then into csv by Pandas. Great progress!
    
    Note: JuPyter notebook is a great tool for tesing!
    
    Future work:
    1. Trimming the dataframe/prompt: don't really need the flashcard index afterall
    2. Formating the flashcard_keyword from a list of strings into a acceptable format by anki
    3. Hooking it up to PDF miner/ Search for other ways to import data from PDF files

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+## Specifying the model that we want from Ollama and the creativity of the output(0-1, 1 for max creativity)
 from langchain_community.llms import Ollama
-from langchain_core.prompts import ChatPromptTemplate
 
 llm = Ollama(model="mistral", temperature = 0.0)
 
+## Prompt template
+from langchain_core.prompts import ChatPromptTemplate
 
 flashcardgen = ChatPromptTemplate.from_messages([
     ("system", """
@@ -25,17 +27,31 @@ flashcardgen = ChatPromptTemplate.from_messages([
     ("user", "{input}")
 ])
 
-from langchain_core.output_parsers import CommaSeparatedListOutputParser
+## JSON parsing
+from typing import List
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+class flashcards(BaseModel):
+    flashcard_index: int = Field(description="The index of the current flashcard with respect to the whole JSON file.")
+    flashcard_topic: str = Field(description="The topic of the flashcard.")
+    flashcard_question: str = Field(description="The question of the flashcard.")
+    flashcard_answer: str = Field(description="The answer of the flashcard.")
+    flashcard_keyword: str = Field(description="The keywords of the flashcard.")
 
-output_parser = CommaSeparatedListOutputParser()
+output_parser = JsonOutputParser(pydantic_object=flashcards)
 
-chain = flashcardgen | llm #| output_parser
+## Make the chain
+chain = flashcardgen | llm | output_parser
 
-print(chain.invoke({"input": """
+deck = chain.invoke({"input": """
 1. 眼的移動由6條骨骼肌控制，即上斜肌（Superior oblique）、下斜肌（Inferior oblique）、上直肌（Superior rectus）、下直肌（Inferior rectus）、內直肌（Medial rectus）和外直肌(Lateral rectus）。這些肌肉協調運動可以使眼球移動，以掃描視野中的物體，保持對焦於目標，並調整眼睛的焦點以適應環境變化rectus）。這些肌肉協調運可以使眼球移動，以掃描視野中的物體，保持對焦於目標，並調整眼睛的焦點以適應環境變化。
 2. 物的或頭部在移動時，眼的移動可以獨立於物體移動之外，使得視網膜上的影像能夠穩定地維持在中央小窩這一區域中，即黃斑中心凹（fovea centralis）。這樣做有助於提高對細微差別的分辨能力，尤其是在低視野的情況下。
 3. 這些肌肉不僅能控制眼球運動，還能參與視覺調適（accommodation）過程，這是指眼睛改變其晶體曲率，以調整焦點以更好地適應距離變化。當人眼這些肌肉不僅能控制眼球運動，還能參與視覺調適（accommodation）過程，這是指眼睛改變其晶體曲率，以調整焦點以更好地適應距離變化。當人眼從遠處看近物時，會透過睫狀肌和睫狀小帶的收縮作用來增加眼的屈光力，使得影像能夠清晰聚焦在黃斑中心凹。
 總之，眼球的運動是由一系列肌肉協調控制的，這些肌肉不僅能移動眼球以對應環境中的物體，還能參與視覺功能的許多其他方面，包括維持穩定清晰的視野以及實現視覺遠處看近物時，會透過睫狀肌和睫狀小帶的收縮作用來增加眼的屈光力，使得影像能夠清晰聚焦在黃斑中心凹。總之，眼球的運動是由一系列肌肉協調控制的，這些肌肉不僅能移動眼球以對應環境中的物體，還能參與視覺功能的許多其他方面，包括維持穩定清晰的視野以及實現視覺調適。
-"""}))
+"""})
 
-#print(llm.invoke("why is the sky blue"))
+print(deck)
+print(type(deck))
+
+for card in deck:
+    print(card[""]
