aa0b5051add13f2c581dbf0d854fc264fffd99a7
Stephen200274 <stephen200274@gmail.com>
2024-02-20 14:47:55 +0800

    Added MyMuPDF for PDF extraction
    Fixed and tested bugs, Try to adjust the output and some prompt engineering

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ flashcardgen = ChatPromptTemplate.from_messages([
     flashcard_question
     flashcard_answer
     flashcard_keyword
+    4. Check the output for json format, and make sure all the format rules of json is met.
     
     Keep the keywords count equal or less than 5.
     
@@ -40,18 +41,38 @@ class flashcards(BaseModel):
 
 output_parser = JsonOutputParser(pydantic_object=flashcards)
 
+## PDF extraction
+import fitz
+
+doc = fitz.open("test.pdf")
+textbook = []
+
+for page in doc:
+    text = page.get_text()
+    textbook.append(text)
+
 ## Make the chain
 chain = flashcardgen | llm | output_parser
+deck = []
 
-deck = chain.invoke({"input": """
-1. 眼的移動由6條骨骼肌控制，即上斜肌（Superior oblique）、下斜肌（Inferior oblique）、上直肌（Superior rectus）、下直肌（Inferior rectus）、內直肌（Medial rectus）和外直肌(Lateral rectus）。這些肌肉協調運動可以使眼球移動，以掃描視野中的物體，保持對焦於目標，並調整眼睛的焦點以適應環境變化rectus）。這些肌肉協調運可以使眼球移動，以掃描視野中的物體，保持對焦於目標，並調整眼睛的焦點以適應環境變化。
-2. 物的或頭部在移動時，眼的移動可以獨立於物體移動之外，使得視網膜上的影像能夠穩定地維持在中央小窩這一區域中，即黃斑中心凹（fovea centralis）。這樣做有助於提高對細微差別的分辨能力，尤其是在低視野的情況下。
-3. 這些肌肉不僅能控制眼球運動，還能參與視覺調適（accommodation）過程，這是指眼睛改變其晶體曲率，以調整焦點以更好地適應距離變化。當人眼這些肌肉不僅能控制眼球運動，還能參與視覺調適（accommodation）過程，這是指眼睛改變其晶體曲率，以調整焦點以更好地適應距離變化。當人眼從遠處看近物時，會透過睫狀肌和睫狀小帶的收縮作用來增加眼的屈光力，使得影像能夠清晰聚焦在黃斑中心凹。
-總之，眼球的運動是由一系列肌肉協調控制的，這些肌肉不僅能移動眼球以對應環境中的物體，還能參與視覺功能的許多其他方面，包括維持穩定清晰的視野以及實現視覺遠處看近物時，會透過睫狀肌和睫狀小帶的收縮作用來增加眼的屈光力，使得影像能夠清晰聚焦在黃斑中心凹。總之，眼球的運動是由一系列肌肉協調控制的，這些肌肉不僅能移動眼球以對應環境中的物體，還能參與視覺功能的許多其他方面，包括維持穩定清晰的視野以及實現視覺調適。
-"""})
+## Chunk the information and run them thru the chain for flashcard generation
+for index in range(len(textbook)):
+    print("page %d: started" % (index + 1))
+    try:
+        deck.append(chain.invoke({"input": textbook[index]})["flashcards"])
+        print("page %d: complete" % (index + 1))
+    except:
+        print("page %d: error" % (index + 1))
 
-print(deck)
-print(type(deck))
+## Pandas dataframe for output to csv for anki acception
 
-for card in deck:
-    print(card[""]
+import pandas as pd
+
+reform_deck = []
+for sec in deck:
+    for c in sec:
+        reform_deck.append(c)
+
+deck_df = pd.DataFrame(data=reform_deck, columns=['flashcard_index', 'flashcard_topic', 'flashcard_question', 'flashcard_answer', 'flashcard_keyword'])
+print(deck_df)
+deck_df.to_csv("output.csv", index=False)
